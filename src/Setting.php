daf61f8858c8592768bb0b2e3440de8caa8b5ac7
Ben Wibrew <benjamin.wibrew@gmail.com>
2017-08-10 15:56:00 +0100

    Add docblocks

--- src/Setting.php
+++ src/Setting.php
@@ -7,6 +7,11 @@ use Illuminate\Support\Facades\Auth;
 
 class Setting extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'name',
         'value',
@@ -14,7 +19,7 @@ class Setting extends Model
     ];
 
     /**
-     * Set the settings name.
+     * Set the settings name after checking for naming styles.
      *
      * @param  string $name
      * @return void
@@ -28,6 +33,16 @@ class Setting extends Model
         $this->attributes['name'] = $name;
     }
 
+    /**
+     * Register a new setting with optional value.
+     *
+     * Authenticated user ID will be assigned to 'updated_by' column.
+     *
+     * @param $name
+     * @param null $value
+     * @param null $user
+     * @return $this|Model
+     */
     public static function register($name, $value = null, $user = null)
     {
         if (!$user) {
@@ -43,6 +58,12 @@ class Setting extends Model
         return $setting;
     }
 
+    /**
+     * Update current setting name.
+     *
+     * @param $name
+     * @return $this
+     */
     public function updateName($name)
     {
         $this->name = $name;
@@ -51,6 +72,13 @@ class Setting extends Model
         return $this;
     }
 
+    /**
+     * Update current setting value.
+     *
+     * @param null $value
+     * @param null $user
+     * @return $this
+     */
     public function updateValue($value = null, $user = null)
     {
         if (!$user) {
@@ -64,21 +92,45 @@ class Setting extends Model
         return $this;
     }
 
+    /**
+     * Get a setting value.
+     *
+     * @param $name
+     * @return mixed
+     */
     public static function getValue($name)
     {
         return Setting::where('name', $name)->pluck('value')->first();
     }
 
+    /**
+     * Get the 'updated_by' user ID.
+     *
+     * @param $name
+     * @return mixed
+     */
     public static function getUpdatedBy($name)
     {
         return Setting::where('name', $name)->pluck('updated_by')->first();
     }
 
+    /**
+     * Get the updated_at timestamp.
+     *
+     * @param $name
+     * @return mixed
+     */
     public static function getWhenUpdated($name)
     {
         return Setting::where('name', $name)->pluck('updated_at')->first();
     }
 
+    /**
+     * Assert the setting name follows the naming style.
+     *
+     * @param $name
+     * @return bool
+     */
     protected function followsNamingStyle($name)
     {
         if (in_array('snake_case', config('sitesettings.naming_styles'))) {
