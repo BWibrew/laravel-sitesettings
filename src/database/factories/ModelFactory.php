4bfe90650b4012a0e8f3c4050d194dddfe54715f
Ben Wibrew <benjamin.wibrew@gmail.com>
2017-08-12 10:56:36 +0100

    Tweak Setting model factory

--- src/database/factories/ModelFactory.php
+++ src/database/factories/ModelFactory.php
@@ -13,9 +13,9 @@ $factory->define(Jamin87\SiteSettings\Tests\Models\User::class, function (Faker\
 
 $factory->define(Jamin87\SiteSettings\Setting::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->word,
-        'scope' => $faker->optional(0.2)->word,
-        'value' => $faker->optional(0.1)->sentence(),
+        'name' => $faker->unique()->word,
+        'scope' => $faker->optional(0.8)->word,
+        'value' => $faker->optional(0.9)->sentence(),
         'updated_by' => function () {
             if (mt_rand(0, 1)) {
                 return factory(Jamin87\SiteSettings\Tests\Models\User::class)->create()->id;
