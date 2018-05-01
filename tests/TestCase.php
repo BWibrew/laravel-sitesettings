a115d8658751d813d598dcddb40a2e886770db22
Ben Wibrew <benjamin.wibrew@gmail.com>
2018-05-01 21:09:26 +0100

    Improve migrations in tests
    Fix build

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,13 +3,13 @@
 namespace BWibrew\SiteSettings\Tests;
 
 use BWibrew\SiteSettings\Tests\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Orchestra\Testbench\TestCase as Orchestra;
 use BWibrew\SiteSettings\Tests\Models\SettingWithMedia;
 
 class TestCase extends Orchestra
 {
-    use RefreshDatabase;
+    use DatabaseMigrations;
 
     /**
      * Setup the test environment.
@@ -18,8 +18,15 @@ class TestCase extends Orchestra
     {
         parent::setUp();
 
+        if(! class_exists('CreateSettingsTable')) {
+            $this->artisan('vendor:publish', ['--provider' => 'BWibrew\SiteSettings\SiteSettingsServiceProvider', '--tag' => 'migrations']);
+        }
+
         $this->withFactories(__DIR__.'/database/factories');
+
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
         $this->loadLaravelMigrations(['--database' => 'sqlite']);
+        $this->artisan('migrate', ['--database' => 'sqlite']);
     }
 
     /**
@@ -46,10 +53,6 @@ class TestCase extends Orchestra
         // Media Library config
         $app['config']->set('medialibrary.custom_url_generator_class', TestUrlGenerator::class);
         $app['config']->set('medialibrary.defaultFilesystem', 'public');
-
-        // Migrations
-        $this->migrateSettingsTable();
-        $this->migrateMediaTable();
     }
 
     /**
@@ -63,6 +66,7 @@ class TestCase extends Orchestra
     {
         return [
             \BWibrew\SiteSettings\SiteSettingsServiceProvider::class,
+            \Orchestra\Database\ConsoleServiceProvider::class,
         ];
     }
 
@@ -78,17 +82,5 @@ class TestCase extends Orchestra
         $parts = explode('/', SettingWithMedia::find($id)->getMedia()->first()->getPath());
 
         return implode('/', array_slice($parts, -2, 2));
-    }
-
-    protected function migrateSettingsTable()
-    {
-        include_once __DIR__.'/../database/migrations/create_settings_table.php.stub';
-        (new \CreateSettingsTable())->up();
-    }
-
-    protected function migrateMediaTable()
-    {
-        include_once __DIR__ . '/database/migrations/create_media_table.php.stub';
-        (new \CreateMediaTable())->up();
     }
 }
